3513d633de307104dec362384a1096c469012ec6
ghddbwns9808 <65646370+ghddbwns9808@users.noreply.github.com>
2023-09-15 16:42:00 +0900

    [Gold IV] Title: 도시 분할 계획, Time: 1936 ms, Memory: 351772 KB -BaekjoonHub

---
+++ 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.java
@@ -0,0 +1,85 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+class Main{
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    static List<ArrayList<Edge>> edges = new ArrayList<>();
+    static PriorityQueue<Edge> pq = new PriorityQueue<>();
+    static int v, e;
+
+    public static void main(String[] args) throws IOException {
+        StringTokenizer tk = new StringTokenizer(br.readLine());
+        v = Integer.parseInt(tk.nextToken());
+        e = Integer.parseInt(tk.nextToken());
+
+        int maxWeight = Integer.MIN_VALUE;
+
+        boolean[] visited = new boolean[v+1];
+
+        for(int i=0; i <= v; i++)
+            edges.add(new ArrayList<>());
+
+        for (int i=0; i<e; i++){
+            tk = new StringTokenizer(br.readLine());
+            int from = Integer.parseInt(tk.nextToken());
+            int to = Integer.parseInt(tk.nextToken());
+            int weight = Integer.parseInt(tk.nextToken());
+
+            edges.get(from).add(new Edge(to, weight));
+            edges.get(to).add(new Edge(from, weight));
+        }
+
+        pq.offer(new Edge(1, 0));
+
+        int total = 0;
+
+        while (!pq.isEmpty()){
+            Edge cur = pq.poll();
+            if(visited[cur.to]) continue;
+
+            visited[cur.to] = true;
+            total += cur.weight;
+            maxWeight = Math.max(maxWeight, cur.weight);
+
+            for (Edge e: edges.get(cur.to)){
+                if (!visited[e.to])
+                    pq.add(e);
+            }
+        }
+
+        bw.write((total - maxWeight) + "");
+        bw.flush();
+    }
+}
+
+class Edge implements Comparable<Edge>{
+    int to;
+    int weight;
+
+    public Edge( int t, int w){
+        this.to = t;
+        this.weight = w;
+    }
+
+    @Override
+    public int compareTo(Edge edge) {
+        return this.weight - edge.weight;
+    }
+
+    @Override
+    public String toString(){
+        return "E[" + to + ", " + weight + "]";
+    }
+}
