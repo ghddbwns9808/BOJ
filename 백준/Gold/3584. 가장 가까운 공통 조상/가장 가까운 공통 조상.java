3fd58367ac046017900024cc4e0f21b39a5a0c7d
ghddbwns9808 <65646370+ghddbwns9808@users.noreply.github.com>
2023-10-16 23:10:19 +0900

    [Gold IV] Title: 가장 가까운 공통 조상, Time: 344 ms, Memory: 30916 KB -BaekjoonHub

--- 백준/Gold/3584. 가장 가까운 공통 조상/가장 가까운 공통 조상.java
+++ 백준/Gold/3584. 가장 가까운 공통 조상/가장 가까운 공통 조상.java
@@ -5,16 +5,16 @@ public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     static int N;
-    static int[] parent;
     static List<Node> tree;
     public static void main(String[] args) throws IOException {
         int T = Integer.parseInt(br.readLine());
         while (T --> 0){
+            //child 노드에 등장한 수를 모두 더할 변수
             int tmpSum = 0;
             N = Integer.parseInt(br.readLine());
-            parent = new int[N+1];
-            tree = new ArrayList<>();
 
+            //tree 초기화
+            tree = new ArrayList<>();
             for (int i=0; i<=N; i++)
                 tree.add(new Node(i));
 
@@ -22,21 +22,28 @@ public class Main {
                 StringTokenizer tk = new StringTokenizer(br.readLine());
                 int p = Integer.parseInt(tk.nextToken());
                 int c = Integer.parseInt(tk.nextToken());
+                //tmpSum에 child 값 더하기
                 tmpSum += c;
 
+                //트리에 값 입력
                 tree.get(p).children.add(c);
                 tree.get(c).parent = p;
             }
 
+            //1 ~ N 까지 수를 더했을 때의 값
             int sum = N%2 == 0 ? (1 + N) * (N/2) : (1 + N) * (N/2) + (N/2)+1;
+            //sum - tmpSum 하면
+            //child 노드로 한번도 등장하지 않은 -> parent가 없는 -> root 노드를 찾을 수 있음
             int root = sum - tmpSum;
 
+            //루트 노드의 depth를 0으로 시작해서 depth 설정하기
             setDepth(root, 0);
 
             StringTokenizer tk = new StringTokenizer(br.readLine());
             int n1 = Integer.parseInt(tk.nextToken());
             int n2 = Integer.parseInt(tk.nextToken());
 
+            //lca(n1, n2)를 무조건 n1의 depth가 n2의 depth보다 낮지 않게 시작하기 위해
             if (tree.get(n1).depth >= tree.get(n2).depth) bw.write(lca(n1, n2) + "\n");
             else bw.write(lca(n2, n1) + "\n");
         }
@@ -52,10 +59,12 @@ public class Main {
     private static int lca(int n1, int n2){
         int a = n1;
         int b = n2;
+        //두 노드의 depth가 일치할 때 까지 a를 위로 올리기
         while (tree.get(a).depth > tree.get(b).depth){
             a = tree.get(a).parent;
         }
 
+        //같은 조상을 찾을 때 까지 위로 올려주기
         while (a != b){
             a = tree.get(a).parent;
             b = tree.get(b).parent;
@@ -76,3 +85,9 @@ class Node{
         this.depth = 0;
     }
 }
+
+/*
+1. lca 알고리즘입니다.
+2. root 노드를 지정하지 않았기 때문에 root 노드를 찾는 과정이 필요합니다.
+3. 이때 노드가 최대 10000개이기 때문에 빠르게 찾을 방법을 생각해봤습니다.
+ */
