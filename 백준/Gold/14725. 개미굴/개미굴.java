539209f354fa5abe07c2b43e730c170ca18ce3ad
ghddbwns9808 <65646370+ghddbwns9808@users.noreply.github.com>
2023-10-21 14:12:20 +0900

    [Gold III] Title: 개미굴, Time: 196 ms, Memory: 15976 KB -BaekjoonHub

--- 백준/Gold/14725. 개미굴/개미굴.java
+++ 백준/Gold/14725. 개미굴/개미굴.java
@@ -10,13 +10,10 @@ public class Main {
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
     static int N;
-    static ArrayList<Node> tree;
 
     public static void main(String[] args) throws IOException {
         N = Integer.parseInt(br.readLine());
-        tree = new ArrayList<>();
-        tree.add(new Node(""));
-
+        //root 노드 생성
         Node root = new Node("");
 
         while (N--> 0){
@@ -24,6 +21,7 @@ public class Main {
             StringTokenizer tk = new StringTokenizer(br.readLine());
             int m = Integer.parseInt(tk.nextToken());
 
+            //입력순대로 차례대로 트라이에 넣기
             for (int i=0; i<m; i++){
                 String s = tk.nextToken();
                 cur.insert(s);
@@ -36,6 +34,7 @@ public class Main {
         bw.flush();
     }
 
+    //문제 출력 조건이 전위표기 방식으로 출력하게 되어있음
     private static void printTrie(Node cur, int depth) throws IOException {
         if (depth != 0){
             for (int i=0; i<2*(depth-1); i++)
@@ -54,9 +53,12 @@ class Node{
 
     public Node(String c){
         this.cur = c;
+        //사전순 정렬이니 TreeMap
         this.child = new TreeMap<>();
     }
 
+    //트라이에 insert하는 함수
+    //child map에 key가 s인 노드가 없다면 넣어라
     public void insert(String s){
         child.putIfAbsent(s, new Node(s));
     }
