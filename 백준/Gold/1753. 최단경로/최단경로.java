0cc601759e7533cf52db03a7650cf02b72cb469b
ghddbwns9808 <65646370+ghddbwns9808@users.noreply.github.com>
2024-07-01 20:52:08 +0900

    [Gold IV] Title: 최단경로, Time: 688 ms, Memory: 108656 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -1,29 +1,39 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
+    static List<ArrayList<Edge>> relations;
+    static int[] ans;
+    static boolean[] vst;
     static int V, E, start;
-    static int[] cost;
-    static boolean[] visited;
-    static PriorityQueue<Edge> pq;
-    static List<ArrayList<Edge>> graph;
+
     public static void main(String[] args) throws IOException {
+        input();
+        solve();
+
+        for (int i=1; i<=V; i++){
+            if (ans[i] == Integer.MAX_VALUE) bw.write("INF\n");
+            else bw.write(ans[i] + "\n");
+        }
+        bw.flush();
+        bw.close();
+    }
+
+    static void input() throws IOException{
         StringTokenizer tk = new StringTokenizer(br.readLine());
         V = Integer.parseInt(tk.nextToken());
         E = Integer.parseInt(tk.nextToken());
-
         start = Integer.parseInt(br.readLine());
-        cost = new int[V+1];
-        visited = new boolean[V+1];
-        Arrays.fill(cost, Integer.MAX_VALUE);
-        graph = new ArrayList<>();
-        pq = new PriorityQueue<>();
+
+        relations = new ArrayList<>();
+        vst = new boolean[V+1];
+        ans = new int[V+1];
+        Arrays.fill(ans, Integer.MAX_VALUE);
 
         for (int i=0; i<=V; i++)
-            graph.add(new ArrayList<>());
+            relations.add(new ArrayList<>());
 
         for (int i=0; i<E; i++){
             tk = new StringTokenizer(br.readLine());
@@ -31,43 +41,42 @@ public class Main {
             int to = Integer.parseInt(tk.nextToken());
             int weight = Integer.parseInt(tk.nextToken());
 
-            graph.get(from).add(new Edge(to, weight));
+            relations.get(from).add(new Edge(to, weight));
         }
 
+    }
+
+    static void solve(){
+        PriorityQueue<Edge> pq = new PriorityQueue();
         pq.offer(new Edge(start, 0));
-        cost[start] = 0;
+        ans[start] = 0;
+
         while (!pq.isEmpty()){
             Edge cur = pq.poll();
 
-            if (visited[cur.to]) continue;
-            visited[cur.to] = true;
+            if (vst[cur.to]) continue;
+            vst[cur.to] = true;
 
-            for (Edge e: graph.get(cur.to)){
-                if(cost[e.to] > cost[cur.to] + e.weight){
-                    cost[e.to] = cost[cur.to] + e.weight;
-                    pq.offer(new Edge(e.to, cost[e.to]));
+            for (Edge nxt: relations.get(cur.to)){
+                if (ans[nxt.to] > ans[cur.to] + nxt.weight){
+                    pq.offer(new Edge(nxt.to, ans[cur.to] + nxt.weight));
+                    ans[nxt.to] = ans[cur.to] + nxt.weight;
                 }
             }
         }
-
-        for (int i=1; i<=V; i++) {
-            if (cost[i] != Integer.MAX_VALUE) bw.write(cost[i] + "\n");
-            else bw.write("INF\n");
-        }
-        bw.flush();
     }
 }
 
 class Edge implements Comparable<Edge>{
     int to, weight;
+
     public Edge(int t, int w){
         this.to = t;
         this.weight = w;
     }
 
-
     @Override
-    public int compareTo(Edge edge) {
-        return weight - edge.weight;
+    public int compareTo(Edge other){
+        return this.weight - other.weight;
     }
 }
