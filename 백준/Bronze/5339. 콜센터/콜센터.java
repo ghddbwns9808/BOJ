adfc2c39cba2be8aa5c97847730f76c29abc867a
ghddbwns9808 <65646370+ghddbwns9808@users.noreply.github.com>
2024-10-07 17:04:36 +0900

    [Bronze V] Title: 콜센터, Time: 100 ms, Memory: 14172 KB -BaekjoonHub

---
+++ 백준/Bronze/5339. 콜센터/콜센터.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    static int ans;
+
+
+    public static void main(String[] args) throws IOException {
+        System.out.println("     /~\\\n" +
+                "    ( oo|\n" +
+                "    _\\=/_\n" +
+                "   /  _  \\\n" +
+                "  //|/.\\|\\\\\n" +
+                " ||  \\ /  ||\n" +
+                "============\n" +
+                "|          |\n" +
+                "|          |\n" +
+                "|          |");
+    }
+}
