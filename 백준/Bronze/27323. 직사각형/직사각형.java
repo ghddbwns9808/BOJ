b6fba5458d834bdbec8b48c6f7369639592db4b7
ghddbwns9808 <65646370+ghddbwns9808@users.noreply.github.com>
2024-10-07 16:57:13 +0900

    [Bronze V] Title: 직사각형, Time: 100 ms, Memory: 14076 KB -BaekjoonHub

---
+++ 백준/Bronze/27323. 직사각형/직사각형.java
@@ -0,0 +1,7 @@
+import java.io.*;
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws IOException {
+        System.out.println(Integer.parseInt(br.readLine()) * Integer.parseInt(br.readLine()));
+    }
+}
